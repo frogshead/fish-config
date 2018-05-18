608a91e326f0078be1a6aa30828abf74741736ba
Mikko Viitamäki <mikko.vitamaki@gmail.com>
2018-05-18 18:51:22 +0300

    use $HOME instead

--- config.fish
+++ config.fish
@@ -1,10 +1,18 @@
 set fish_greeting '>>-´´´*>'
 
 # Check if esp32 dev environment installed 
-if test -d /home/mikko/esp/xtensa-esp32-elf/
-    set -gx PATH $PATH /home/mikko/esp/xtensa-esp32-elf/bin
+if test -d $HOME/esp/xtensa-esp32-elf/
+    set -gx PATH $PATH $HOME/esp/xtensa-esp32-elf/bin
 end
 
-if test -d /home/mikko/esp/esp-idf
-    set -xU IDF_PATH /home/mikko/esp/esp-idf
+if test -d $HOME/esp/esp-idf
+    set -xU IDF_PATH $HOME/esp/esp-idf
+end
+
+if test -d $HOME/esp/esp-adf
+    set -xU ADF_PATH $HOME/esp/esp-adf
+end
+
+if test -d $HOME/.cargo/
+    set -gx PATH $PATH $HOME/.cargo/bin/
 end
